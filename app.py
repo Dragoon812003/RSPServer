37539412b3cad3cc603fd1f32b87ca989373d04c
Muhammad Kamkoriwala <kamkoriwalamuhammad@gmail.com>
2022-12-20 14:26:29 +0530

    ijdfiasdjif

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from ultrasonic import *
 app=Flask(__name__)
 camera = cv2.VideoCapture(0)
 
-arm_moving = False
 IR1, IR2, IL1, IL2, AM1, AM2 = setup()
 GPIO_TRIGGER, GPIO_ECHO = ultrasonic_setup()
 
@@ -42,23 +41,21 @@ def move():
     if request.method == "POST":
         direction = request.form['direction']
         if direction == "forward":
-            # go_forward(IR1, IR2, IL1, IL2)
+            go_forward(IR1, IR2, IL1, IL2)
             pass
         elif direction == "left":
-            # go_left(IR1, IR2, IL1, IL2)
+            go_left(IR1, IR2, IL1, IL2)
             pass
         elif direction == "right":
-            # go_right(IR1, IR2, IL1, IL2)
+            go_right(IR1, IR2, IL1, IL2)
             pass
         elif direction == "backward":
-            # go_backward(IR1, IR2, IL1, IL2)
+            go_backward(IR1, IR2, IL1, IL2)
             pass
         elif direction == "stop":
-            # stop(IR1, IR2, IL1, IL2)
+            stop(IR1, IR2, IL1, IL2)
             pass
-        # print(direction)
-        # dist = distance(GPIO_TRIGGER, GPIO_ECHO)
-        # print(dist)
+        print(direction)
         return json.dumps({'status': 'OK'})
     else:
         return redirect('/')
--- test.js
+++
@@ -1,9 +0,0 @@
-let data = {
-    "distance": 78.4584454,
-}
-
-data = JSON.stringify(data)
-
-parsedData = JSON.parse(data)
-ultraSonicDist = parsedData.distance
-console.log(`${ultraSonicDist.toFixed(2)}mm`)
